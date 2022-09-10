18ed629e88a301e46e95d88276348f21b8ada32c
Ai Shadhin <aishadhin.ac@gmail.com>
2022-09-10 20:18:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import SingleProperty from './SingleProperty';
 
 function App() {
+
+  const [property, setProperty] = useState([]);
+
+  useEffect(() => {
+    fetch('data.json')
+      .then(res => res.json())
+      .then(data => setProperty(data))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto py-16'>
+      <h2 className='text-xl capitalize font-bold text-center py-3'>Property search with filtering system</h2>
+      <form className='border border-1 p-3 flex justify-between'>
+        <select className="select select-bordered w-full lg:w-[20%]">
+          <option disabled selected>Location</option>
+          <option>Miami</option>
+          <option>Hollywood</option>
+        </select>
+        <select className="select select-bordered w-full lg:w-[20%]">
+          <option disabled selected>Move-In Date</option>
+          <option>13/9/2022</option>
+          <option>14/9/2022</option>
+        </select>
+        <select className="select select-bordered w-full lg:w-[20%]">
+          <option disabled selected>Price in $</option>
+          <option>1000 - 1400</option>
+          <option>1401 - 1800</option>
+        </select>
+        <select className="select select-bordered w-full lg:w-[20%]">
+          <option disabled selected>Property Type</option>
+          <option>House</option>
+          <option>Office</option>
+        </select>
+        <input type="submit" value='Search' className='btn btn-primary' />
+      </form>
+      <div className='grid grid-cols-3 gap-3'>
+        {
+          property.map(singleProperty => <SingleProperty singleProperty={singleProperty} key={singleProperty.id}></SingleProperty>)
+        }
+      </div>
     </div>
   );
 }
---
+++ src/SingleProperty.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const SingleProperty = ({ singleProperty }) => {
+    const { name, img, location, type, date, price } = singleProperty
+    return (
+        <div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src={img} alt="property" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center">
+                    <h2 className="card-title">{name}</h2>
+                    <div className='grid grid-cols-2 gap-x-6'>
+                        <p>Location: {location}</p>
+                        <p>Type: {type}</p>
+                        <p>Date: {date}</p>
+                        <p>Price: ${price}</p>
+                    </div>
+                    <div className="card-actions">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SingleProperty;
