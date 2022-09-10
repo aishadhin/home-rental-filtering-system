9bda8b94f500bbd9f4f55bc9f5b81f46f1731d29
Ai Shadhin <aishadhin.ac@gmail.com>
2022-09-10 21:58:44 +0600

    get input data

--- src/App.js
+++ src/App.js
@@ -6,31 +6,56 @@ function App() {
 
   const [property, setProperty] = useState([]);
 
+
   useEffect(() => {
     fetch('data.json')
       .then(res => res.json())
       .then(data => setProperty(data))
   }, [])
+
+
+  const handleSubmit = e =>{
+    e.preventDefault();
+    const location = e.target.location.value;
+    const date = e.target.date.value;
+    const price = e.target.price.value;
+    const type = e.target.type.value;
+    console.log(location , date , price , type);
+  }
+
+
+
+
+  // searchTerms = (person) => {
+  //   return person.name === "john" && person.dinner === "sushi";
+  // }
+
+
+
+
+
+
+
   return (
     <div className='container mx-auto py-16'>
       <h2 className='text-xl capitalize font-bold text-center py-3'>Property search with filtering system</h2>
-      <form className='border border-1 p-3 flex justify-between'>
-        <select className="select select-bordered w-full lg:w-[20%]">
+      <form onSubmit={handleSubmit} className='border border-1 p-3 flex justify-between'>
+        <select name='location' className="select select-bordered w-full lg:w-[20%]">
           <option disabled selected>Location</option>
           <option>Miami</option>
           <option>Hollywood</option>
         </select>
-        <select className="select select-bordered w-full lg:w-[20%]">
+        <select name='date' className="select select-bordered w-full lg:w-[20%]">
           <option disabled selected>Move-In Date</option>
           <option>13/9/2022</option>
           <option>14/9/2022</option>
         </select>
-        <select className="select select-bordered w-full lg:w-[20%]">
+        <select name='price' className="select select-bordered w-full lg:w-[20%]">
           <option disabled selected>Price in $</option>
           <option>1000 - 1400</option>
           <option>1401 - 1800</option>
         </select>
-        <select className="select select-bordered w-full lg:w-[20%]">
+        <select name='type' className="select select-bordered w-full lg:w-[20%]">
           <option disabled selected>Property Type</option>
           <option>House</option>
           <option>Office</option>
